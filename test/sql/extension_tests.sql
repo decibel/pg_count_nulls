8c4557d3876ad71cd98673bb45bdc6692856a326
Jim Nasby <Jim.Nasby@BlueTreble.com>
2015-04-13 19:23:42 -0500

    Add test for renaming and dropping extension

---
+++ test/sql/extension_tests.sql
@@ -0,0 +1,27 @@
+\set ECHO none
+
+\set schema public
+\i test/helpers/setup.sql
+
+\set schema schema_to_load_count_nulls
+CREATE SCHEMA :schema;
+ALTER EXTENSION pg_count_nulls SET SCHEMA :schema;
+
+\i test/core/functions.sql
+
+CREATE FUNCTION shutdown__drop_all
+() RETURNS SETOF text LANGUAGE plpgsql AS $body$
+BEGIN
+    RETURN NEXT lives_ok(
+        $$DROP EXTENSION pg_count_nulls$$
+    );
+
+    RETURN NEXT lives_ok(
+        $$DROP SCHEMA schema_to_load_count_nulls$$
+    );
+END
+$body$;
+
+--SET client_min_messages = debug;
+
+SELECT * FROM runtests( '_null_count_test'::name );
