6eb39bbcef99e8ea9909a49fda6448ba279862ce
Jim Nasby <Jim.Nasby@BlueTreble.com>
2015-04-30 13:51:10 -0500

    Reorganize

--- sql/count_nulls.sql
+++ sql/count_nulls.sql
@@ -7,20 +7,21 @@ CREATE OR REPLACE FUNCTION null_count(
     FROM unnest( $1 ) a
 $body$;
 
-CREATE OR REPLACE FUNCTION not_null_count(
-  VARIADIC argument anyarray
-) RETURNS bigint LANGUAGE sql IMMUTABLE AS $body$
-  SELECT sum( CASE WHEN a IS NOT NULL THEN 1 ELSE 0 END )
-    FROM unnest( $1 ) a
-$body$;
-
-
 CREATE OR REPLACE FUNCTION null_count(
   argument json
 ) RETURNS bigint LANGUAGE sql IMMUTABLE AS $body$
 SELECT count(*)
   FROM json_each_text( $1 ) a
   WHERE value IS NULL
+$body$;
+
+
+
+CREATE OR REPLACE FUNCTION not_null_count(
+  VARIADIC argument anyarray
+) RETURNS bigint LANGUAGE sql IMMUTABLE AS $body$
+  SELECT sum( CASE WHEN a IS NOT NULL THEN 1 ELSE 0 END )
+    FROM unnest( $1 ) a
 $body$;
 
 CREATE OR REPLACE FUNCTION not_null_count(
